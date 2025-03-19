4fda4a65a8cfe782327051eac60ec34f2299dfbb
Patrick Armstrong <patrick.james.1998@gmail.com>
2025-03-20 10:37:32 +1100

    prep for tests

---
+++ tests/test_supaernova/test_parity/__init__.py
@@ -0,0 +1,2 @@
+# Copyright 2025 Patrick Armstrong
+"""Test whether we recover the same behaviour as the original SuPAErnova code."""
---
+++ tests/test_supaernova/test_parity/test_data_step_parity.py
@@ -0,0 +1,4 @@
+# Copyright 2025 Patrick Armstrong
+"""Test whether the data step behaviour matches that of the original SuPAErnova code."""
+
+import suPAErnova as snPAE
